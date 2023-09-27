5de471b7ab7eaa1e75e35382da78de83cabfdc5c
Peter Bækgaard <pbakga21@student.aau.dk>
2023-09-27 13:02:07 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import ThemeSelect from '$lib/theme-select.svelte';
+</script>
+
+<div class="navbar bg-base-300">
+	<div class="navbar-start">
+		<a class="btn btn-ghost normal-case text-xl">pbaekgaard</a>
+	</div>
+	<div class="navbar-end">
+		<ThemeSelect />
+	</div>
+</div>
