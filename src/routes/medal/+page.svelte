0afe52dea750158b39e9a1235741f0dfc168ffad
pbaekgaard <pbakga21@student.aau.dk>
2024-01-15 12:09:22 +0100

    added direct download

--- src/routes/medal/+page.svelte
+++ src/routes/medal/+page.svelte
@@ -1,9 +1,30 @@
 <script>
 	import { goto } from '$app/navigation';
-	const url =
-		'https://medal.tv/games/csgo/clips/1eaSPSaFyROqlg/d1337spERrC3?invite=cr-MSxuVngsNzAxNjA0Mzcs?mobilebypass=true';
-	let output = '';
+	let container;
+	let isLoading = false;
+	let inputValue = '';
+	async function DownloadMP4(url, clipName) {
+		const downloadUrl = 'https://corsproxy.org/?' + encodeURIComponent(url);
+		var scr = document.createElement('script');
+		scr.innerHTML = `
+		var dlButton = document.createElement('Button');
+		dlButton.innerHTML = 'Download Medal Clip';
+		dlButton.className = 'btn btn-outline btn-accent';
+		dlButton.addEventListener('click', buttonDownload);
+		dlButtonContainer = document.getElementById('downloadButtonContainer');
+		dlButtonContainer.appendChild(dlButton);
+		async function buttonDownload() {
+			fetch('${downloadUrl}').then(res => res.blob()).then(blob => {var url = window.URL.createObjectURL(blob); var a = document.createElement('a'); a.href = url; a.download = '${clipName}'; document.body.appendChild(a); a.click(); a.remove();});
+		}
+		`;
+		container.appendChild(scr);
+		isLoading = false;
+	}
+
 	async function gotoDownload(event) {
+		var downloadButtonContainer = document.getElementById('downloadButtonContainer');
+		downloadButtonContainer.innerHTML = '';
+		isLoading = true;
 		const url = event.target.value + '?mobilebypass=true';
 		const response = await fetch('/medal/clip', {
 			method: 'POST',
@@ -13,9 +34,9 @@
 			}
 		});
 
-		const { clipLink } = await response.json();
+		const { clipLink, clipName } = await response.json();
 		if (clipLink) {
-			goto(clipLink);
+			DownloadMP4(clipLink, clipName);
 		}
 	}
 
@@ -27,20 +48,27 @@
 </script>
 
 <div class="flex justify-center w-full h-1/2 flex-col items-center">
-	<input
-		type="text"
-		id="urlinput"
-		placeholder="ENTER MEDAL.TV SHARE URL"
-		class="input input-bordered input-lg input-accent h-20 w-4/6 max-w-6xl border-2 rounded-md placeholder:text-center"
-		on:paste={async (event) => {
-			await setTimeout(() => {
-				gotoDownload(event);
-			}, 10);
-		}}
-		on:keyup={(event) => {
-			onEnter(event);
-		}}
-	/>
+	<div class="flex flex-row w-full justify-center">
+		<input
+			type="text"
+			id="urlinput"
+			placeholder="ENTER MEDAL.TV SHARE URL"
+			class="input input-bordered input-lg input-accent h-20 w-4/6 max-w-6xl border-2 rounded-md placeholder:text-center"
+			on:paste={async (event) => {
+				await setTimeout(() => {
+					gotoDownload(event);
+				}, 10);
+			}}
+			on:keyup={(event) => {
+				onEnter(event);
+			}}
+			bind:value={inputValue}
+		/>
+	</div>
+	<div bind:this={container} id="downloadButtonContainer" class="py-5">
+		{#if isLoading == true}
+			<span class="loading loading-spinner text-accent" />{:else}{/if}
+	</div>
 	<ul class="italic text-gray-400 pt-5">
 		<li>1. Copy Medal.tv Share link</li>
 		<li>2. Paste link here</li>
--- src/routes/medal/clip/+server.ts
+++ src/routes/medal/clip/+server.ts
@@ -6,7 +6,15 @@ export async function POST({ request, cookies }) {
 		.then((response) => response.text())
 		.then((html) => {
 			const root = parse(html);
-			return root.querySelector("meta[property='og:video:url']")['_attrs']['content'];
+			let clip = root.querySelector("meta[property='og:video:url']")['_attrs']['content'];
+			let clipName = root.querySelector("meta[property='og:title']")['_attrs']['content'];
+			clipName = clipName.substring(0, clipName.length - 24);
+			clipName = clipName + '.mp4';
+			return {
+				clipUrl: clip,
+				clipName: clipName
+			};
 		});
-	return json({ clipLink: req }, { status: 200 });
+	const reqUrl = await fetch(req.clipUrl).then((res) => res.url);
+	return json({ clipLink: reqUrl, clipName: req.clipName }, { status: 200 });
 }
